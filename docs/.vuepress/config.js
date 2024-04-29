7e65f9ae8439aebc53288489b256b82bf59f1fc2
Jinmengxin <jinmengxinwork@163.com>
2024-04-29 13:15:16 +0800

    yml 10

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Hello JDreamUI',
     description: 'Just playing around',
-    base:"/",
+    base:"/jdream-ui/",
     title:"jdream-ui",
     themeConfig: {
     nav: [
